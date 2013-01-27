f54f57cbc5f9aafca777c6995f5cd1daed33155d
unknown <milie@EN62356.endava.net>
2013-01-27 23:25:09 +0200

    Initial commit'

---
+++ src/test/java/com/insidecoding/sos/sample/TestGoogleSearch.java
@@ -0,0 +1,43 @@
+package com.insidecoding.sos.sample;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.insidecoding.sos.junit.AbstractSoSBase;
+
+/**
+ * Test class for some simple functionalities.
+ * 
+ * @author ludovicianul
+ * 
+ */
+public class TestGoogleSearch extends AbstractSoSBase {
+
+	private static final Logger LOG = Logger.getLogger(TestGoogleSearch.class);
+
+	@Test
+	public void testSimpleSearch() throws Exception {
+		LOG.info("Driver received: " + driver);
+
+		driver.get("http://google.com");
+
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("What is Selenium");
+
+		helper.waitForElementPresent(By.id("ires"), 10);
+	}
+
+	@Override
+	protected void doAdditionalSetUp() {
+		// no need
+
+	}
+
+	@Override
+	protected void doAdditionalTearDown() {
+		// no need
+
+	}
+}
